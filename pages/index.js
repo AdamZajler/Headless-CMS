f4a233dd6d8220c0e6c5ccb48dff3b473f0cac31
Adam Zajler <adamzajler01@gmail.com>
2022-03-18 16:06:28 +0100

    Added page title query & slug translation based on local json

--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,46 @@ import fetcher from "../../../headless-wordpress/lib/fetcher";
 import parse from "html-react-parser";
 import getMeta from "../lib/getMeta";
 import { GET_MAIN_MENU } from "../lib/wordpress/menu";
+import { GET_PAGE_DATA } from "../lib/wordpress/page";
 import { MainMenu } from "../components/wordpress/mainMenu";
-import { useRouter } from "next/router";
+import { getPageSlug } from "../lib/getPageSlug";
 
-export default function Home({ mainMenu, meta }) {
+export default function Home({ mainMenu, meta, page, pageSlug }) {
+	// Parse sting data to HTML
 	const metaData = parse(meta);
+
+	console.log("slug strony w WP zaciągniety z jsona: ", pageSlug);
 	return (
 		<div>
 			<Head>{metaData}</Head>
 
 			<MainMenu menu={mainMenu} />
 
-			<MainMenu menu={mainMenu} />
-
-			<main></main>
+			<main>
+				<h1>{page.title}</h1>
+			</main>
 		</div>
 	);
 }
 
 export async function getStaticProps(context) {
+	// Get translated page slug based on page DATABASE ID (EN)
+	let pageSlug = getPageSlug(3935, context.locale)
+	
 	let menuID;
 	let language = context.locale;
 	language == "pl" ? (menuID = "119") : (menuID = "159");
+	// const nameOfPage =
 	const variables = {
 		// For mainMenu
 		menuID: menuID,
+		pageName: language == "pl" ? "strona-glowna" : "home-page",
 	};
 	const mainMenuResponse = await fetcher(GET_MAIN_MENU, { variables });
 	const mainMenu = mainMenuResponse.menu;
 	const metaResponse = await getMeta("home-page");
 	const meta = metaResponse ? metaResponse : "RankMath response empty";
-	return { props: { mainMenu, meta } };
+	const pageDataResponse = await fetcher(GET_PAGE_DATA, { variables });
+	const page = pageDataResponse.page;
+	return { props: { mainMenu, meta, page, pageSlug } };
 }
