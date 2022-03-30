48abcd5dff03d4636ab2ca3004068ee1fb3bb7ed
Adam Zajler <adamzajler01@gmail.com>
2022-03-30 18:51:02 +0200

    Added all data in topBar & created fetcher for themeSettings

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,18 @@ import parse from "html-react-parser";
 import getMeta from "../lib/getMeta";
 import { GET_SINGLE_MENU } from "../lib/wordpress/menu";
 import { GET_PAGE_DATA } from "../lib/wordpress/page";
+import { GET_THEME_SETTINGS } from "../lib/wordpress/head";
 import { MainMenu } from "../components/wordpress/mainMenu";
 import { getPageSlug } from "../lib/getPageSlug";
 import { getMenu } from "../lib/getMenu";
+import { getThemeSetting } from "../lib/getThemeSetting";
 import { Container } from "../components/structure/container";
 import { ContainerFull } from "../components/structure/ContainerFull";
 
-export default function Home({ meta, page, menus }) {
+export default function Home({ meta, page, menus, themeSettings }) {
+	// data from themeSetting (customizer)
+	const favicon = getThemeSetting(themeSettings, "ThemeSettings_Favicon").link;
+
 	// Filter for primary menu
 	const mainMenu = menus.filter((single) => {
 		if (single.type == "primary") {
@@ -23,14 +28,16 @@ export default function Home({ meta, page, menus }) {
 
 	return (
 		<div>
-			<Head>{metaData}</Head>
+			<Head>
+				{metaData}
+				<link rel="icon" type="image/x-icon" href={favicon}></link>
+			</Head>
 
 			<MainMenu mainMenu={mainMenu} />
 
 			<Container>
 				<h2 className="text-white">COŚ</h2>
 			</Container>
-
 			<main>
 				<ContainerFull>
 					<h1 className="font-bold text-primary">{page.title}</h1>
@@ -63,7 +70,9 @@ export async function getStaticProps(context) {
 
 	const metaResponse = await getMeta(pageSlug);
 	const meta = metaResponse ? metaResponse : "RankMath response empty";
+	const themeSettingsResponse = await fetcher(GET_THEME_SETTINGS);
+	const themeSettings = themeSettingsResponse.themeSettings;
 	const pageDataResponse = await fetcher(GET_PAGE_DATA, { variables });
 	const page = pageDataResponse.page;
-	return { props: { meta, page, menus } };
+	return { props: { meta, page, menus, themeSettings } };
 }
